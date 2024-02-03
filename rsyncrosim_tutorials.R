b2f24ae0b395ce09e3f01a2afab025736e5eb875
d-romualdi <doriana.romualdi@mail.utoronto.ca>
2024-02-03 14:30:08 -0500

    Connecting to R SyncroSim using session()

--- rsyncrosim_tutorials.R
+++ rsyncrosim_tutorials.R
@@ -4,9 +4,25 @@
 # DR
 
 ### Running rsyncrosim tutorials ###
+# must install SyncroSim software first - DONE
 
 # https://syncrosim.github.io/rsyncrosim/
 
 # install rsyncrosim
 install.packages("rsyncrosim")
+
+#load package
+library(rsyncrosim)
+
+
+### Introduction to rsyncrosim ###
+# https://syncrosim.github.io/rsyncrosim/articles/a01_rsyncrosim_vignette_basic.html
+
+### SyncoSim Package: helloworldTime
+# package to introduce timesteps to Syncrosim modeling 
+# takes 2 user inputs, runs through linear model, and returns y value as output
+# Input: m (slope), and b (intercept)
+# Output: model y = mt + b where t is time
+
+
 
