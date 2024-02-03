2dba3a7e4967c33d323429ce5421df1a3dbb532a
d-romualdi <doriana.romualdi@mail.utoronto.ca>
2024-02-03 14:36:53 -0500

    Installing helloworldTime package

--- rsyncrosim_tutorials.R
+++ rsyncrosim_tutorials.R
@@ -24,5 +24,25 @@ library(rsyncrosim)
 # Input: m (slope), and b (intercept)
 # Output: model y = mt + b where t is time
 
+# Connecting to R SyncroSim using session
+# connects to SyncroSim software
 
+mySession <- session("C:/Program Files/SyncroSim") # created Session based on SyncroSim install folder
+mySession # Displaying Session object
+
+version(mySession) #2.5.6
+
+### Installing SyncroSim Packages using addPackages()
+
+# get list of installed packages
+package()
+
+# see which packages you do not have installed yet
+availablePackages <- package(installed = FALSE)
+head(availablePackages)
+
+# Install helloworldTime
+addPackage("helloworldTime")
+
+package() #checking if helloworldTime is included in package list now - it is!
 
