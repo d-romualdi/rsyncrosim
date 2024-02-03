af784f1b1c792158d1b226f582ec4d698506c855
d-romualdi <doriana.romualdi@mail.utoronto.ca>
2024-02-03 14:15:33 -0500

    Installing rsyncrosim

---
+++ rsyncrosim_tutorials.R
@@ -0,0 +1,12 @@
+### February 3rd 2024 ###
+
+# R 4.3.2
+# DR
+
+### Running rsyncrosim tutorials ###
+
+# https://syncrosim.github.io/rsyncrosim/
+
+# install rsyncrosim
+install.packages("rsyncrosim")
+
